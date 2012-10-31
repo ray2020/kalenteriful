5302bbe1e0839ed573e0ff80e067a5e0d9c86c4d
Mikko Niemikorpi <mikefulli@gmail.com>
2012-11-01 00:14:37 +0200

    Changes to project settings
     - Added settings.py and local_settings.py loading to main file
     - Removed local_settings.py importing from settings.py

--- kalenteriful.py
+++ kalenteriful.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config.from_pyfile('settings.py')
+app.config.from_pyfile('local_settings.py', silent=True)
+
 @app.route('/')
 def hello_world():
 	return 'Hello World!'
--- settings.py
+++ settings.py
@@ -1,7 +1,1 @@
 # General project settings
-
-# Try importing from local settings file
-try:
-	from local_settings import *
-except ImportError:
-	pass
