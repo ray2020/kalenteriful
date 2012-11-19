ffa8c3868777e50ec0d463930cfd94b744d209c0
Mikko Niemikorpi <mikefulli@gmail.com>
2012-11-19 20:37:10 +0200

    PEP8 formatting of Python files

--- kalenteriful.py
+++ kalenteriful.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from flask.ext.security import Security, SQLAlchemyUserDatastore
+from flask import Flask, flash, redirect, render_template, url_for, request
+from flask.ext.security import Security, SQLAlchemyUserDatastore, LoginForm, login_user, logout_user
+from flask.ext.login import LoginManager, login_required
 from models.shared import db
 from models.accounts import Role, User
 
@@ -19,9 +20,53 @@ db.init_app(app)
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
+# Setup Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return User.get(userid)
+
+
+# Setup database
+@app.before_first_request
+def db_setup():
+    db.create_all()
+
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if request.method == "POST" and "username" in request.form:
+        username = request.form["username"]
+        password = request.form["password"]
+        remember = request.form.get("remember", "no") == "yes"
+
+        user = User.query.filter_by(username=username, password=password).first()
+        # TODO Hash password for check
+        if user:
+            if login_user(user, remember=remember):
+                flash("Logged in!")
+                return redirect(request.args.get("next") or url_for("index"))
+            else:
+                flash("Sorry, but you could not log in.")
+        else:
+            flash("Invalid username.")
+    return render_template("login.html", form=form)
+
+
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
 @app.route('/')
-def hello_world():
-	return 'Hello World!'
+def index():
+    return render_template("index.html")
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
